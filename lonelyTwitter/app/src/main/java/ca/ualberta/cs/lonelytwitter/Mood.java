ff25c06c2987313c7c6976deac07284f68b983ac
Whiteologist <342395558@qq.com>
2017-09-14 16:36:08 -0600

    Add files via upload

---
+++ lonelyTwitter/app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by haoming3 on 9/13/17.
+ */
+
+public abstract class Mood implements Tweetable {
+    private String message;
+    private Date date;
+
+
+}
